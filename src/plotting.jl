535825c052946363cbd4a84686a2ecb546ad68ad
Cas van der Oord <casv2@cam.ac.uk>
2018-09-29 16:42:23 +0100

    added IP_pdf

--- src/plotting.jl
+++ src/plotting.jl
@@ -1,9 +1,9 @@
 module Plotting
 
 using Reexport
-using StaticArrays, JuLIP, Plots, NBodyIPs
+using NBodyIPs, DataFrames, NBodyIPFitting, StaticArrays, JuLIP, Plots
 
-export slice_plot, force_plot, energy_plot
+export force_plot, energy_plot, IP_table, IP_info, IP_plot, IP_pdf
 
 function unfold(A)
     V = []
@@ -17,38 +17,182 @@ function unfold(A)
     V
 end
 
-function slice_plot(IP)
-    if Dict(IP)["components"][4]["D"]["__id__"] == "BondAngleDesc"
-        E0 = IP.components[1].E0
+function IP_table(info)
+    df = DataFrame(config = [], E = [], F = [], V = [])
 
-        IP_comp = NBodyIPs.bodyorder.(IP.components)[2:end] #dropping E0 for 1body
+    for key1 in keys(info["errors"]["rmse"])
+        df2 = DataFrame(config = [key1], E = [info["errors"]["rmse"][key1]["E"]],
+                       F = [info["errors"]["rmse"][key1]["F"]], V = [info["errors"]["rmse"][key1]["V"]])
+        append!(df, df2)
+    end
 
-        str = " body/cutoff: "
-        for i in IP_comp
-           str *= (string(i) * "/" * string(IP.components[i].D.cutoff.rcut)[1:4] * ", ")
+    names!(df, [Symbol("config type"), Symbol("E (eV)"), Symbol("F (eV/A)"), Symbol("V (eV/A)")])
+
+    rmse_table = reprmime("text/latex", df)
+
+    df = DataFrame(config = [], E = [], F = [], V = [])
+
+    for key1 in keys(info["errors"]["mae"])
+        df2 = DataFrame(config = [key1], E = [info["errors"]["mae"][key1]["E"]],
+                       F = [info["errors"]["mae"][key1]["F"]], V = [info["errors"]["mae"][key1]["V"]])
+        append!(df, df2)
+    end
+
+    names!(df, [Symbol("config type"), Symbol("E (eV)"), Symbol("F (eV/A)"), Symbol("V (eV/A)")])
+
+    mae_table = reprmime("text/latex", df)
+
+    df_1 = DataFrame(config = [], weight = [])
+
+    for key in keys(info["configweights"])
+        df2 = DataFrame(config = key, weight = info["configweights"][key])
+        append!(df_1, df2)
+    end
+
+    df_2 = DataFrame(data = [], weight = [])
+
+    for key in keys(info["dataweights"])
+        df2 = DataFrame(data = key, weight = info["dataweights"][key])
+        append!(df_2, df2)
+    end
+
+    y = reprmime("text/latex", df_1)
+    x = reprmime("text/latex", df_2)
+
+    rmse = "RMSE"
+    mae = "MAE"
+
+    dbpath = replace(split(info["dbpath"], "/")[end], "_" => " ")
+
+    E0 = info["E0"]
+
+    if length(info["regularisers"]) == 0
+        regulariser = "None"
+    end
+
+    b = "\\documentclass[preview]{standalone}
+    \\usepackage{graphicx}
+    \\usepackage{caption}
+    \\captionsetup[figure]{font=small}
+    \\begin{document}
+    \\begin{table}
+    \\caption{$rmse}
+    \\begin{center}
+    \\scalebox{1}{
+    $rmse_table}
+    \\end{center}
+    \\end{table}
+    \\begin{table}
+    \\caption{$mae}
+    \\begin{center}
+    \\scalebox{1}{
+    $mae_table}
+    \\end{center}
+    \\end{table}
+    \\begin{center}
+    \\begin{tabular}{ll}
+    \\begin{tabular}{ccc}
+    $x
+    \\end{tabular}
+    &
+    \\begin{tabular}{ccc}
+    $y
+    \\end{tabular}
+    \\end{tabular}
+    \\newline
+    \\newline
+    database: $dbpath
+    \\newline
+    E0 = $E0
+    \\newline
+    regulariser : $regulariser
+    \\newline
+    \\end{center}
+    \\end{document}"
+
+    write("IP_table.tex", b)
+
+    run(`pdflatex IP_table.tex`)
+end
+
+function IP_plot(IP; return_plot = true)
+    IPs = NBodyIPs.bodyorder.(IP.components)[2:end]
+
+    rr = linspace(1, 8, 200)
+    θ0 = acos(-1/3)
+
+    p = plot( yaxis=([-1,1]) )
+
+    j = 2
+
+    if length(find(IPs .== 2)) > 1
+        V2a(r) = IP.components[j](r)
+        j += 1
+        V2b(r) = IP.components[j](r)
+        plot!(p, rr, V2b.(rr) + V2a.(rr), label="V2a + V2b")
+    else
+        V2(r) = IP.components[j](r)
+        plot!(p, rr, V2.(rr), label="V2")
+    end
+
+    j += 1
+
+    for i in deleteat!(IPs, findin(IPs, [2]))
+        println(i)
+        if i == 3
+            try
+                println(j)
+                V3(r1,r2,r3) = IP.components[j]( SVector(r1,r2,r3) )
+                plot!(p, rr, V3.(rr,rr,rr), label="V3 (BL)")
+            catch
+                println(j)
+                V3(r1,r2,θ) = IP.components[j]( (SVector(r1, r2), SVector(cos(θ)*r1*r2) ) )
+                plot!(p, rr, V3.(rr, rr, θ0), label = "V3 (BA)")
+            end
+        elseif i == 4
+            try
+                V4(r1,r2,r3,r4,r5,r6) = IP.components[j]( SVector(r1,r2,r3,r4,r5,r6) )
+                plot!(p, rr, V4.(rr,rr,rr,rr,rr,rr), label="V4 (BL)")
+            catch
+                V4(r1,r2,r3, θ1, θ2, θ3) = IP.components[j]( (SVector(r1, r2, r3), SVector(θ1, θ2, θ3)) )
+                plot!(p, rr, V4.(rr, rr, rr, θ0, θ0, θ0), label="V4 (BA)")
+            end
         end
+        j += 1
+    end
 
-        #V2/V3/V4
-        V2(r) = IP.components[2](r)
-        V3(r1,r2,θ) = IP.components[3]( (SVector(r1, r2), SVector(cos(θ)*r1*r2) ) )
-        #V4(r1, r2, θ) = IP.components[4]( (SVector(r1, r2), SVector(cos(θ)*r1*r2) ) )
+    r0 = rnn(:Si)
 
-        θ0 = acos(-1/3)
-        rr2 = linspace(1, 8, 200)
-        rr3 = linspace(1, 8, 200)
+    vline!([r0], label="r0", color="black")
+    xlabel!("Interatomic distance (A)")
+    ylabel!("Energy (eV)")
 
-        P1 = plot( yaxis = ([-2, 0.7], ) )
-        plot!(P1, rr2, V2.(rr2), label = "V2")
-        plot!(P1, rr3, V3.(rr3, rr3, θ0), label = "V3(r,r,t0)")
-        title!("e0=" * string(E0)[1:6] * str[1:end-2] * " BA")
-        xlabel!("Interatomic distance (A)")
-        ylabel!("Energy")
+    if return_plot == false
+        savefig("IP_plot.pdf")
+    else
+        display(p)
     end
 end
 
-function force_plot(test_data, IP; s = 50)
-    data = sortcols(hcat([[split(test_data[i].configtype, ":")[1], test_data[i].D["F"], unfold(forces(IP, Atoms(test_data[i])))] for i in  1:s:length(test_data)]...))
+function IP_info(info; return_plot = true)
+    b = bar(legend=:false)
 
+    for key in keys(info["numconfigs"])
+        bar!(b, [key], [info["numconfigs"][key]])
+    end
+
+    xlabel!("Configs in fit")
+    ylabel!("Number of configs")
+
+    if return_plot == false
+        savefig("IP_info.pdf")
+    else
+        display(b)
+    end
+end
+
+function force_plot(test_data, IP; s = 50, return_plot = true)
+    data = sortcols(hcat([[split(test_data[i].configtype, ":")[1], test_data[i].D["F"], unfold(forces(IP, JuLIP.Atoms(test_data[i])))] for i in  1:s:length(test_data)]...))
     uniq_config_sel = unique(data[1,:])
 
     p1 = plot()
@@ -64,7 +208,6 @@ function force_plot(test_data, IP; s = 50)
 
     plot_max = findmax(unfold(data[3,:]))[1]
     plot_min = findmin(unfold(data[3,:]))[1]
-
     for uniq_config in uniq_config_sel
         indices = find(data[1,:] .== uniq_config)
         min, max = findmin(indices)[1], findmax(indices)[1]
@@ -72,13 +215,19 @@ function force_plot(test_data, IP; s = 50)
         p2 = scatter!(p2, unfold(data[2, min:max]), abs.(unfold(data[3, min:max]) - unfold(data[2, min:max])), label=uniq_config, legend=false, yaxis=(:log10, (0.00005,2)))
     end
 
-    p1 = plot!(p1, [plot_min, plot_max], [plot_min, plot_max], color="black")
+    P = plot!(p1, [plot_min, plot_max], [plot_min, plot_max], color="black")
 
-    plot(p1, p2, layout=(1,2), size=(1400,700))
+    P = plot(p1, p2, layout=(1,2), size=(1400,700))
+
+    if return_plot == false
+        savefig("IP_force.pdf")
+    else
+        display(P)
+    end
 end
 
-function energy_plot(test_data, IP; s = 50)
-    data = sortcols(hcat([[split(test_data[i].configtype, ":")[1], (test_data[i].D["E"][1]/length(test_data[i].at.Z)), (energy(IP, Atoms(test_data[i]))/length(test_data[i].at.Z))] for i in  1:s:length(test_data)]...))
+function energy_plot(test_data, IP; s = 50, return_plot = true)
+    data = sortcols(hcat([[split(test_data[i].configtype, ":")[1], (test_data[i].D["E"][1]/length(test_data[i].at.Z)), (energy(IP, JuLIP.Atoms(test_data[i]))/length(test_data[i].at.Z))] for i in  1:s:length(test_data)]...))
 
     uniq_config_sel = unique(data[1,:])
 
@@ -105,7 +254,26 @@ function energy_plot(test_data, IP; s = 50)
 
     p1 = plot!(p1, [plot_min, plot_max], [plot_min, plot_max], color="black")
 
-    plot(p1, p2, layout=(1,2), size=(1400,700))
+    P = plot(p1, p2, layout=(1,2), size=(1400,700))
+
+    if return_plot == false
+        savefig("IP_energy.pdf")
+    else
+        display(P)
+    end
+
+end
+
+function IP_pdf(IP, info, test_data)
+    IP_table(info)
+    IP_plot(IP, return_plot = false)
+    IP_info(info, return_plot = false)
+    force_plot(test_data, IP, s=50, return_plot = false)
+    energy_plot(test_data, IP, s=50, return_plot = false)
+
+    #run(`convert IP_energy.png IP_energy.pdf`)
+    #run(`convert IP_force.png IP_force.pdf`)
+    run(`pdfjoin IP_plot.pdf IP_info.pdf IP_table.pdf IP_energy.pdf IP_force.pdf --outfile IP_pdf.pdf --rotateoversize false`)
 end
 
 end
